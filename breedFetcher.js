66521fe60c543d4c17f051dced985da20ea07a64
prashanthk02 <prashanth.k369@gmail.com>
2022-03-18 00:43:48 -0400

    updated

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,15 +2,16 @@ const request = require('request');
 
 const fetchBreedDescription = function(breedName, callback) {
   request(`https://api.thecatapi.com/v1/breeds/search?name=${breedName}`, (error, response, body) => {
-    const data = JSON.parse(body);
-    if (data.length === 0) {
-      callback('breed not found', null);
-    }
     if (error) {
-      callback(error, null);
+      return callback(error, null);
+    }
+
+  const data = JSON.parse(body);
+    if (data.length === 0) {
+      return callback('breed not found', null);
     }
     if (data.length !== 0) {
-      callback(null, data[0].description);
+      return callback(null, data[0].description);
     }
   });
 };
